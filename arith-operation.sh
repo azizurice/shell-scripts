dba98b3aa4888ccf365334e9b88adf80b39f436b
Azizur Rahman <azizur.ice@gmail.com>
2020-03-21 19:38:53 -0700

    Added arithmetic operation

---
+++ arith-operation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#We provide you with expressions containing +,-,*,^, / and parenthesis. None of the numbers in the expression involved will exceed .
+#Your task is to evaluate the expression and display the correct output rounding upto  decimal places.
+
+read expression
+echo `printf "%.3f" $(($expression))`
+
+
--- triangle.sh
+++ triangle.sh
@@ -7,11 +7,15 @@
 
 #How to run: enter three sides of the triable as arguments. e.g. ./triable.sh 2 3 5 
 
-side1="${1}"
-side2="${2}"
-side3="${3}"
+#side1="${1}"
+#$side2="${2}"
+#side3="${3}"
 
-echo "Sides of triangle are: ${side1} ${side2} ${side3}"
+#echo "Sides of triangle are: ${side1} ${side2} ${side3}"
+
+read side1
+read side2
+read side3
 
 if [ "${side1}" -eq "${side2}" ] && [ "${side2}" -eq "${side3}" ]
 then
