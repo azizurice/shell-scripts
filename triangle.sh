7511af85868b8fadd38e91dbb5ca3609e3a987bf
Mohammad Zubayer <zubayer@gmail.com>
2020-03-21 22:00:46 -0400

    Captured arguments in variables

--- triangle.sh
+++ triangle.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #Given three integers (, , and ) representing the three sides of a triangle, identify whether the triangle is Scalene, Isosceles, or Equilateral.
 
+#How to run: enter three sides of the triable as arguments. e.g. ./triable.sh 2 3 5 
 
+side1="${1}"
+side2="${2}"
+side3="${3}"
+
+echo "Sides of triangle are: ${side1} ${side2} ${side3}"
